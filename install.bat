66957aee147e272d00035bb5527f03030793ab55
a.lam.tuan <lamtuananh278@gmail.com>
2019-04-08 11:08:11 +0200

    released version 1.0

---
+++ install.bat
@@ -0,0 +1,1 @@
+mvn install:install-file -DgroupId=lataa -DartifactId=jnibuilder -Dversion=1.0 -Dfile=jnibuilder.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
